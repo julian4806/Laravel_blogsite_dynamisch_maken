a6fd9133b39da37a82c1329e317069ac127e681a
Julian van Husen <210549@student.glu.nl>
2022-12-15 13:13:58 +0100

    edited tutorial

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,8 @@ use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-// // Get all the listings
-
 Route::get('/', [PostController::class, 'index'])->name('blogs');
-
 Route::get('/search', [PostController::class, 'search'])->name('search');
-
-
 Route::get('/listings/{listing}', [PostController::class, 'viewblog']);
-
 Route::get('/create', [PostController::class, 'viewCreate']);
-
 Route::post('/create', [PostController::class, 'create']);
